b610c73fd9d3d7f58898fb31a23d18ede708a27f
octane23 <95738756+octane23@users.noreply.github.com>
2023-04-18 16:44:05 +0800

    Add files via upload

---
+++ login/auth.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'users';
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+  
+     //Hash the password
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Check if the email and hashed password match a record in the database
+    $sql = "SELECT * FROM users WHERE email='$email'";
+    $result = mysqli_query($conn, $sql);
+  
+    if ($result && mysqli_num_rows($result) > 0) {
+      $user = mysqli_fetch_assoc($result);
+  
+      if (password_verify($password, $user['password'])) {
+        // Password is correct, start a session
+        session_regenerate_id();
+        $_SESSION['loggedin'] = TRUE;
+        $_SESSION['email'] = $email;
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['access_level'] = $user['access_level'];
+        
+        // Redirect to the appropriate page based on access level
+    if ($user['access_level'] == 0) {
+      // Guest user, redirect to login page
+      header('Location: index.php');
+    } else if ($user['access_level'] == 1) {
+      // Regular user, redirect to student details page
+      header('Location: ../reservationform/index.php');
+    } else if ($user['access_level'] == 2) {
+      // Administrator, redirect to user management page
+      header('Location: user_management.php');
+    }
+      
+      } else {
+        // Password is not correct, show an error message
+        $error = "Incorrect password";
+        header('Location: index.php?error=' . urlencode($error));
+      }
+    } else {
+      // Email is not found, show an error message
+      $error = "Email not found";
+      header('Location: index.php?error=' . urlencode($error));
+    }
+  }
+
+  mysqli_close($conn);
+?>
